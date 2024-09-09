d8fc435823aea30922567e2ee5063bd690242194
AshweejaKR <ashweejabhat23@gmail.com>
2024-09-10 00:33:01 +0530

    updated broker_stub & test.py

--- test.py
+++ test.py
@@ -5,28 +5,22 @@ Created on Fri Jun 21 23:36:16 2024
 @author: ashwe
 """
 
-from broker import *
-# from broker_stub import *
-
-delay = 1.2
+# from broker import *
+from broker_stub import *
 
 def ltp_test(ticker, exchange):
     c = 0
     res = "NA"
     print("starting ...")
-    time.sleep(delay)
     obj = broker()
     while True:
         x = obj.get_current_price(ticker, exchange)
         try:
             print("Current price: ", x)
             c = c + 1
-            print("waiting ...")
-            time.sleep(delay)
 
             if c > 2:
                 print("THE END ...")
-                time.sleep(delay)
                 del obj
                 res = "PASS"
                 break
@@ -46,56 +40,67 @@ def run_test(ticker, exchange):
     res = "NA"
     try:
         print("running the test ...")
-        time.sleep(delay)
-
         obj = broker()
-        
-        data = obj.get_user_data()
-        print(data)
-        print("----------------------------------\n")
-        time.sleep(delay)
-        
-        amt = obj.get_margin()
-        print(amt)
-        print("----------------------------------\n")
-        time.sleep(delay)
         
         x = obj.get_current_price(ticker, exchange)
         print("Current price: ", x)
         print("----------------------------------\n")
-        time.sleep(delay)
     
-        duration = 5
-        data = obj.hist_data_daily(ticker, duration, exchange)
-        print(data, '\n')
+        x = obj.get_entry_exit_price(ticker)
+        print("Entry price: ", x)
         print("----------------------------------\n")
-        time.sleep(delay)
-    
-        data = obj.hist_data_intraday(ticker, exchange)
-        print(data)
+
+        x = obj.get_entry_exit_price(ticker, True)
+        print("Exit price: ", x)
         print("----------------------------------\n")
-        time.sleep(delay)
+
+        amt = obj.get_margin()
+        print("Available Amount: ", amt)
+        print("----------------------------------\n")
         
         oid = obj.place_buy_order(ticker, 10, exchange)
-        print(oid)
+        print("Order ID: ", oid)
         print("----------------------------------\n")
-        time.sleep(delay)
 
         stat = obj.get_oder_status(oid)
-        print(stat)
+        print("Order ID status: ", stat)
         print("----------------------------------\n")
-        time.sleep(delay)
         
         oid = obj.place_sell_order(ticker, 10, exchange)
-        print(oid)
+        print("Order ID: ", oid)
         print("----------------------------------\n")
-        time.sleep(delay)
 
         stat = obj.get_oder_status(oid)
-        print(stat)
+        print("Order ID status: ", stat)
         print("----------------------------------\n")
-        time.sleep(delay)
         
+        data = obj.get_user_data()
+        print("User data: ", data)
+        print("----------------------------------\n")
+        
+        duration = 5
+        data = obj.hist_data_daily(ticker, duration, exchange)
+        print("Hist daily data: ", data, '\n')
+        print("----------------------------------\n")
+    
+        data = obj.hist_data_intraday(ticker, exchange)
+        print("Hist Intraday data: ", data, '\n')
+        print("----------------------------------\n")
+
+        qty = 10
+        data = obj.verify_holding(ticker, qty)
+        print("verify holding: ", data)
+        print("----------------------------------\n")
+
+        data = obj.verify_position(ticker, qty)
+        print("verify entry position: ", data)
+        print("----------------------------------\n")
+        
+        data = obj.verify_position(ticker, qty, True)
+        print("verify exit position: ", data)
+        print("----------------------------------\n")
+        
+        print(dir(obj))
         del obj
         res = "PASS"
         print("running the test: DONE")
@@ -104,3 +109,8 @@ def run_test(ticker, exchange):
         res = "FAIL"
 
     print("test result: {}".format(res))
+
+# ticker = "NIFTYBEES-EQ"
+# exchange = "NSE"
+# run_test(ticker, exchange)
+# ltp_test(ticker, exchange)
