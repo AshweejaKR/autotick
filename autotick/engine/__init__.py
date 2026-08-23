7b6aa7d42f2c0ba8fe07e59e9d574ac72f02f8ff
AshweejaKR <103932786+AshweejaKR@users.noreply.github.com>
2026-08-23 23:39:33 +0530

    Phase 7: add engine package

---
+++ autotick/engine/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 23 23:02:28 2026
+
+@author: ashwe
+
+Mode-neutral engine components for AutoTick.
+"""
+
+from autotick.engine.market_session import CalendarSessionManager
+
+__all__ = ["CalendarSessionManager"]
