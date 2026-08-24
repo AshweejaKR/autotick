f14a3a9943cc9ec5e43fe812dea576cdfeaa2b6c
AshweejaKR <103932786+AshweejaKR@users.noreply.github.com>
2026-08-24 21:12:45 +0530

    export simulated providers

--- autotick/providers/__init__.py
+++ autotick/providers/__init__.py
@@ -7,6 +7,10 @@ Created on Sun Aug 23 23:02:28 2026
 Provider selection, bundling, and shared broker sessions for AutoTick.
 """
 
+from autotick.providers.brokers.simulated import (
+    SimulatedAccountProvider,
+    SimulatedExecutionProvider,
+)
 from autotick.providers.factory import ModeMapping, ProviderBundle, ProviderFactory
 from autotick.providers.historical import HistoricalProvider
 from autotick.providers.session_pool import BrokerSession, SessionPool
@@ -18,4 +22,6 @@ __all__ = [
     "ProviderBundle",
     "ProviderFactory",
     "SessionPool",
+    "SimulatedAccountProvider",
+    "SimulatedExecutionProvider",
 ]
