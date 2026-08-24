83220208115d35f4e68bc30e0ba1f262ec060379
AshweejaKR <103932786+AshweejaKR@users.noreply.github.com>
2026-08-24 21:38:11 +0530

    export AngelOne adapters

---
+++ autotick/providers/brokers/angelone/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 19:56:33 2026
+
+@author: ashwe
+"""
+
+from autotick.providers.brokers.angelone.account import AngelOneAccountProvider
+from autotick.providers.brokers.angelone.session import AngelOneSession
+
+__all__ = ["AngelOneAccountProvider", "AngelOneSession"]
